fcf648526a604e2c5ae3da69b19ce35746c38010
Parthiv-Sarker <parthivsdevelopment@gmail.com>
2024-05-26 12:32:40 +0530

    Update in jwtAuth middleware.

--- src/middlewares/jwtAuth.middleware.js
+++ src/middlewares/jwtAuth.middleware.js
@@ -2,7 +2,14 @@ import jwt from "jsonwebtoken";
 
 const authService = async (req, res, next) => {
 	try {
-		const token = req.cookies?.accessToken;
+		const authHeader = req.headers.authorization;
+		if (!authHeader) {
+			return res
+				.status(401)
+				.json({ message: "Authentication token is missing" });
+		}
+
+		const token = authHeader.split(" ")[1];
 		if (!token) {
 			return res
 				.status(401)
@@ -12,7 +19,7 @@ const authService = async (req, res, next) => {
 		const decodedToken = await jwt.verify(token, process.env.JWT_SECRET);
 		const userId = decodedToken.userId;
 
-		req.userId = userId; // You can attach userId to req for later use in other middleware or routes
+		req.userId = userId; // Attach userId to req for later use in other middleware or routes
 		next(); // Call next() to pass control to the next middleware
 	} catch (error) {
 		return res.status(401).json({ message: "Invalid token" });
